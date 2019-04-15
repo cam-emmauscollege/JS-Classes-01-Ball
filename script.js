e7ec5d2c3e3c9ed6a51eaea4159dbfc17233fe5a
A.P. Cammeraat <ariepieter@yaranga.nl>
2019-04-15 11:04:49 +0200

    Eerste commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//import {Ball} from 'ball.js';
+
+var bal = new Ball(300, 200, 4, 4);
+
+function setup() {
+  //bal = new Ball(300, 200, 4, 4);
+  createCanvas(800, 450); // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  background("blue"); // Kleur de achtergrond blauw, zodat je het kunt zien
+  console.log(bal);
+  console.log("setup klaar"); // berichtje voor debuggen
+}
+
+function draw() {
+  background("black");
+
+  
+  bal.update();
+  bal.display();
+}
